fe79e96d08188bf123189fe7253c7795b636ad31
Olle Linderos <olle@linderos.nu>
2021-05-16 01:38:21 +0200

    Add border

--- config.h
+++ config.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1; /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Source Code Pro:size=13"
@@ -15,8 +15,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#D8DEE9" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 4;
+static unsigned int columns    = 6;
+static const unsigned int border_width = 4;
 
 /*
  * Characters not considered part of a word while deleting words
