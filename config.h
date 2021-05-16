92512b0e1769bf82cf0b98d9ee5aaf8ed99050e9
Olle Linderos <olle@linderos.nu>
2021-05-16 12:57:05 +0200

    Apply `navhistory` patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1; /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Source Code Pro:size=13"
+	"Source Code Pro:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -20,6 +20,9 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines      = 3;
 static unsigned int columns    = 8;
 static const unsigned int border_width = 0;
+static unsigned int maxhist    = 64;
+static int histnodup           = 1;	/* if 0, record repeated histories */
+
 
 /*
  * Characters not considered part of a word while deleting words
