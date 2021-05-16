9ee0fcb9dfacd7c06b90298b79bf57541502818a
Olle Linderos <olle@linderos.nu>
2021-05-16 02:05:42 +0200

    Apply `highlight` patch

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int fuzzy = 1; /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
@@ -12,12 +12,14 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
 	[SchemeSel] = { "#D8DEE9", "#4C566A" },
+	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#D8DEE9" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 3;
 static unsigned int columns    = 8;
-static const unsigned int border_width = 4;
+static const unsigned int border_width = 0;
 
 /*
  * Characters not considered part of a word while deleting words
